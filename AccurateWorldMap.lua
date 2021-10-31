15175fdc6e79eec84045a299b9b2d9936712f5a6
Thal-J <irgmisg4nxc9@beconfidential.com>
2021-10-31 16:03:25 +0000

    add the rift and craglorn wayshrines

--- AccurateWorldMap.lua
+++ AccurateWorldMap.lua
@@ -7,8 +7,6 @@ local AWM = {
 	defaults = {}, --put default variables here
 }
 
-
-
 local tiles = {
     "Art/maps/tamriel/Tamriel_0.dds",
     "Art/maps/tamriel/Tamriel_1.dds",
@@ -29,47 +27,117 @@ local tiles = {
 }
 
 
+-- Table of all the wayshrines we want to move, sorted by map (zone). Inner zone names are just for readability and do not actually affect zones themselves.
+-- Some wayshrines have been renamed to be more consistent and lore friendly, this is denoted by the "name=blah" attribute.
+
+-- Wayshrine template: |[x] = { xN = x, yN = y }, -- |
+
+local globalWayshrines = {
+
+  -- Tamriel Map --
+  [1] = { 
+
+    -- Bleakrock Isle --
+    [172] = { xN = 0.613, yN = 0.236 }, -- Bleakrock Isle Wayshrine
+
+    -- Eastmarch --
+    [87] = { xN = 0.585, yN = 0.258 }, -- Windhelm Wayshrine
+    [88] = { xN = 0.567, yN = 0.281 }, -- Fort Morvunskar Wayshrine
+    [89] = { xN = 0.581, yN = 0.279 }, -- Kynesgrove Wayshrine
+    [90] = { xN = 0.571, yN = 0.269 }, -- Voljar Meadery Wayshrine
+    [91] = { xN = 0.544, yN = 0.284 }, -- Cradlecrush Wayshrine
+    [92] = { xN = 0.544, yN = 0.308 }, -- Fort Amol Wayshrine
+    [93] = { xN = 0.573, yN = 0.304 }, -- Wittestadr Wayshrine
+    [94] = { xN = 0.583, yN = 0.323 }, -- Mistwatch Wayshrine
+    [95] = { xN = 0.610, yN = 0.313 }, -- Jorunn's Stand Wayshrine
+    [96] = { xN = 0.600, yN = 0.306 }, -- Logging Camp Wayshrine
+    [97] = { xN = 0.614, yN = 0.297 }, -- Skuldafn Wayshrine
+    [195] = { xN = 0.621, yN = 0.320 }, -- Direfrost Keep Dungeon
+    [389] = { xN = 0.552, yN = 0.281 }, -- Frostvault Dungeon
+    
+    -- The Rift --
+    [109] = { xN = 0.603, yN = 0.366 }, -- Riften Wayshrine
+    [111] = { xN = 0.616, yN = 0.388 }, -- Trollhetta Wayshrine
+    [112] = { xN = 0.615, yN = 0.399 }, -- Trollhetta Summit Wayshrine 
+    [187] = { xN = 0.626, yN = 0.387 }, -- Blessed Crucible Dungeon
+    [120] = { xN = 0.620, yN = 0.375 }, -- Fullhelm Fort Wayshrine
+    [110] = { xN = 0.595, yN = 0.374 }, -- Skald's Retreat Wayshrine
+    [113] = { xN = 0.568, yN = 0.372 }, -- Honrich Tower Wayshrine
+    [119] = { xN = 0.549, yN = 0.363 }, -- Ragged Hills Wayshrine
+    [118] = { xN = 0.541, yN = 0.353 }, -- Nimalten Wayshrine
+    [117] = { xN = 0.531, yN = 0.355 }, -- Taarengrav Wayshrine
+    [116] = { xN = 0.533, yN = 0.340 }, -- Geirmund's Hall Wayshrine 
+    [114] = { xN = 0.592, yN = 0.345 }, -- Fallowstone Hall Wayshrine 
+    [115] = { xN = 0.573, yN = 0.345 }, -- Northwind Mine Wayshrine 
+    
 
 
--- Table of all zones & related data that we want to move
-local zoneData = {
-
-    -- Wayshrine template: [ [x] = { xN = x, yN = y }, -- ]
-
-    -- if disabled = true, set normalised x and y to 0 and return disabled
+    -- Western Skyrim --
+    [424] = { xN = 0.403, yN = 0.156 }, -- Icereach Dungeon
+    [434] = { xN = 0.408, yN = 0.186 }, -- Kyne's Aegis Trial
 
 
-    -- Eastmarch
-    [13] = {  
-        wayshrines = {
-          [87] = { xN = 0.585, yN = 0.258 }, -- Windhelm Wayshrine
-          [88] = { xN = 0.567, yN = 0.281 }, -- Fort Morvunskar Wayshrine
-          [89] = { xN = 0.581, yN = 0.279 }, -- Kynesgrove Wayshrine
-          [90] = { xN = 0.571, yN = 0.269 }, -- Voljar Meadery Wayshrine
-          [91] = { xN = 0.544, yN = 0.284 }, -- Cradlecrush Wayshrine
-          [92] = { xN = 0.544, yN = 0.308 }, -- Fort Amol Wayshrine
-          [93] = { xN = 0.573, yN = 0.304 }, -- Wittestadr Wayshrine
-          [94] = { xN = 0.583, yN = 0.323 }, -- Mistwatch Wayshrine
-          [95] = { xN = 0.610, yN = 0.313 }, -- Jorunn's Stand Wayshrine
-          [96] = { xN = 0.600, yN = 0.306 }, -- Logging Camp Wayshrine
-          --[97] = { disabled = true }, -- Skuldafn Wayshrine
-          [195] = { xN = 0.621, yN = 0.320 }, -- Direfrost Keep Dungeon
-          [389] = { xN = 0.552, yN = 0.281 } -- Frostvault Dungeon
-        },        
-        zoneBlobData = {
-          texture = "AccurateWorldMap/blobs/tamriel-eastmarch.dds",
-          xN = 0.4,
-          yN = 0.4,
-        },
-        zonePolygonData = {
-          -- generate polygon based on texture? edge detection? (for each zone that would be slooow on startup)
-          -- way to move existing polygons? by zoneID?
-        },
-    },
 
-};
+    -- Craglorn --
+    [217] = { xN = 0.379, yN = 0.374 }, -- Seeker's Archive Wayshrine
+    [341] = { xN = 0.405, yN = 0.348 }, -- Fang Lair Dungeon
+    [225] = { xN = 0.392, yN = 0.377 }, -- Spellscar Wayshrine 
+    [220] = { xN = 0.387, yN = 0.387 }, -- Belkarth Wayshrine 
+    [326] = { xN = 0.410, yN = 0.394 }, -- Bloodroot Forge Dungeon 
+    [226] = { xN = 0.401, yN = 0.359 }, -- Mountain Overlook Wayshrine 
+    [227] = { xN = 0.421, yN = 0.367 }, -- Inazzur's Hold Wayshrine
+    [229] = { xN = 0.420, yN = 0.382 }, -- Elinhir Wayshrine 
+    [231] = { xN = 0.428, yN = 0.377 }, -- Aetherian Archive Trial 
+    [332] = { xN = 0.449, yN = 0.357 }, -- Falkreath Hold Dungeon 
+    [233] = { xN = 0.334, yN = 0.338 }, -- Dragonstar Wayshrine 
+    [270] = { xN = 0.335, yN = 0.324 }, -- Dragonstar Arena Dungeon
+    [219] = { xN = 0.354, yN = 0.373 }, -- Sandy Path Wayshrine
+    [235] = { xN = 0.388, yN = 0.350 }, -- Valley of Scars Wayshrine 
+    [218] = { xN = 0.344, yN = 0.353 }, -- Shada's Tear Wayshrine 
+    [234] = { xN = 0.362, yN = 0.349 }, -- Skyreach Wayshrine 
+    [230] = { xN = 0.344, yN = 0.377 }, -- Hel Ra Citadel Trial
+    [232] = { xN = 0.366, yN = 0.330 }, -- Sanctum Ophidia Trial 
 
-local globalWayshrines = {}
+
+
+    
+    -- Cyrodiil --
+    [201] = { xN = 0.509, yN = 0.593, name = "Western Elsweyr Gate Wayshrine" }, -- Western Elsweyr Wayshrine
+    [200] = { xN = 0.556, yN = 0.594, name = "Eastern Elsweyr Gate Wayshrine" }, -- Eastern Elsweyr Wayshrine
+    [202] = { xN = 0.622, yN = 0.410, name = "Northern Morrowind Gate Wayshrine" }, -- Northern Morrowind Wayshrine
+    [203] = { xN = 0.643, yN = 0.455, name = "Southern Morrowind Gate Wayshrine" }, -- Southern Morrowind Wayshrine
+    [170] = { xN = 0.449, yN = 0.411, name = "Northern Hammerfell Gate Wayshrine" }, -- Northern Hammerfell Wayshrine
+    [199] = { xN = 0.429, yN = 0.449, name = "Southern Hammerfell Gate Wayshrine" }, -- Southern Hammerfell Wayshrine
+
+    [236] = { xN = 0.542, yN = 0.475 }, -- Imperial City Prison Dungeon
+    [247] = { xN = 0.536, yN = 0.486 }, -- White Gold Tower Dungeon
+
+  },
+
+  -- Cyrodiil Map --
+  [14] = {
+
+    [202] = { name = "Northern Morrowind Gate Wayshrine" }, -- Northern Morrowind Wayshrine
+    [203] = { name = "Southern Morrowind Gate Wayshrine" }, -- Southern Morrowind Wayshrine
+    [170] = { name = "Northern Hammerfell Gate Wayshrine" }, -- Northern Hammerfell Wayshrine
+    [199] = { name = "Southern Hammerfell Gate Wayshrine" }, -- Southern Hammerfell Wayshrine
+    [200] = { name = "Eastern Elsweyr Gate Wayshrine" }, -- Eastern Elsweyr Wayshrine
+    [201] = { name = "Western Elsweyr Gate Wayshrine" }, -- Western Elsweyr Wayshrine
+
+    [236] = { xN = 0.523, yN = 0.382 }, -- Imperial City Prison Dungeon
+    [247] = { xN = 0.497, yN = 0.428 }, -- White Gold Tower Dungeon
+  },
+
+  -- Western Skyrim Map --
+  [38] = {
+    [434] = { xN = 0.442, yN = 0.193 }, -- Kyne's Aegis
+  }
+
+  --TODO: add earthforge to the reach map as well
+
+}
+    
+    
 
 local enabled = true
 local spoilers = false -- Set this to true if you want the map containing spoilers by default
@@ -183,28 +251,8 @@ local function YourCustomData2(x, y)
 end
 
 
-local function parseWayshrines()
-
-  for mapID, mapData in pairs(zoneData) do
-    local wayshrines = mapData.wayshrines
-    
-    if (wayshrines ~= nil) then
-      for wayshrineID, wayshrineData in pairs(wayshrines) do
-        globalWayshrines[wayshrineID] = wayshrineData
-      end
-    end
-  end
-end
 
 
-
--- upon loading a map, eso iterates through GetFastTravelInfo and loads all the wayshrine data
--- you need to go through your wayshrine list in the order that it is being iterated through
--- to do this, you need to compile all of your wayshrines in order, from 1 to 400 odd
--- this means you need an init function that parses your zonedata Wayshrine stuff
--- and adds it to a new global wayshrine object in the order that getfasttravelnodes is iterated through
--- so you will need to sort the list too
--- then in the getfasttravelinfo function, simply do (does globalwayshrine[nodeIndex] exist? if so, load the data)
 
 local function MoveZones()
   
@@ -224,38 +272,30 @@ local function MoveZones()
     local known, name, normalizedX, normalizedY, icon, glowIcon, poiType, isLocatedInCurrentMap, linkedCollectibleIsLocked = zos_GetFastTravelNodeInfo(nodeIndex)
     local disabled = false
 
-    
-    
-    if globalWayshrines[nodeIndex] ~= nil then
-      normalizedX = globalWayshrines[nodeIndex].xN
-      normalizedY = globalWayshrines[nodeIndex].yN
+    --d("Currently Selected Node: "..name.." ("..nodeIndex..")")
+
+    if globalWayshrines[GetCurrentMapIndex()] ~= nil then
+
+      mapIndex = GetCurrentMapIndex()
+
+      if globalWayshrines[mapIndex][nodeIndex] ~= nil then
+
+        if globalWayshrines[mapIndex][nodeIndex].xN ~= nil then
+          normalizedX = globalWayshrines[mapIndex][nodeIndex].xN
+        end
+
+        if globalWayshrines[mapIndex][nodeIndex].yN ~= nil then
+          normalizedY = globalWayshrines[mapIndex][nodeIndex].yN
+        end
+
+        if globalWayshrines[mapIndex][nodeIndex].name ~= nil then
+          name = globalWayshrines[mapIndex][nodeIndex].name
+        end
+
+      end
+
     end
 
-
-    -- for id, data in pairs(globalWayshrines) do
-    --   d(id)
-    -- end
-
-    if nodeIndex == 424 then -- Icereach
-      normalizedX = 0.403
-      normalizedY = 0.156
-    end
-
-    --d("Test")
-
-    --d(nodeIndex)
-
-
-    -- if (not wayshrineData.disabled) 
-    --   then
-
-
-    --   else
-    --   normalisedX = 0
-    --   normalisedY = 0
-    --   disabled = true
-
-    -- end
 
     return known, name, normalizedX, normalizedY, icon, glowIcon, poiType, isLocatedInCurrentMap, linkedCollectibleIsLocked, disabled
 
@@ -263,92 +303,15 @@ local function MoveZones()
 
 end
 
--- GetFastTravelNodeInfo = function(nodeIndex)
--- 	local known, name, normalizedX, normalizedY, icon, glowIcon, poiType, isLocatedInCurrentMap, linkedCollectibleIsLocked = zos_GetFastTravelNodeInfo(nodeIndex)
--- 	local disabled = false
-  
-  
---   if GetCurrentMapIndex() == 1 then -- Check to see if we are inside the Tamriel map
 
---     -- Cyrodiil --
-    
---     if nodeIndex == 201 then --Western Elsweyr Wayshrine
---       normalizedX = 0.509
---       normalizedY = 0.593
---       name = "Western Elsweyr Gate Wayshrine"
---     end
-    
---     if nodeIndex == 200 then --Eastern Elsweyr Wayshrine
---       normalizedX = 0.556
---       normalizedY = 0.594
---       name = "Eastern Elsweyr Gate Wayshrine"
---     end
-    
---     if nodeIndex == 202 then --Northern Morrowind Wayshrine
---       normalizedX = 0.622
---       normalizedY = 0.410
---       name = "Northern Morrowind Gate Wayshrine"
---     end
-    
---     if nodeIndex == 203 then --Southern Morrowind Wayshrine
---       normalizedX = 0.643
---       normalizedY = 0.455
---       name = "Southern Morrowind Gate Wayshrine"
---     end
-    
---     if nodeIndex == 170 then --Northern Hammerfell Wayshrine
---       normalizedX = 0.449
---       normalizedY = 0.411
---       name = "Northern Hammerfell Gate Wayshrine"
---     end
-    
---     if nodeIndex == 199 then --Southern Hammerfell Wayshrine
---       normalizedX = 0.429
---       normalizedY = 0.449
---       name = "Southern Hammerfell Gate Wayshrine"
---     end
-    
-    
-    
   
---     -- Bleakrock Isle --
     
---     if nodeIndex == 172 then --Bleakrock Isle Wayshrine
---       normalizedX = 0.613
---       normalizedY = 0.236
---     end
+
     
+
+
     
-    
---     -- Dungeons
-    
---     if nodeIndex == 424 then -- Icereach
---       normalizedX = 0.403
---       normalizedY = 0.156
---     end
-    
---     if nodeIndex == 236 then -- Imperial City Prison
---       normalizedX = 0.542
---       normalizedY = 0.475
---     end
-    
---     if nodeIndex == 247 then -- White Gold Tower
---       normalizedX = 0.536
---       normalizedY = 0.486
---     end
-    
---     if nodeIndex == 341 then -- Fang Lair
---       normalizedX = 0.405
---       normalizedY = 0.348
---     end
-    
-    
---     -- Trials
-    
---     if nodeIndex == 434 then -- Kyne's Aegis
---       normalizedX = 0.408
---       normalizedY = 0.186
---     end
+
     
     
 --     -- Houses
@@ -366,57 +329,38 @@ end
 --   end
   
   
---   if GetCurrentMapIndex() == 38 then -- Check to see if we are inside the Western Skyrim zone
-    
---     if nodeIndex == 434 then -- Kyne's Aegis
---       normalizedX = 0.442
---       normalizedY = 0.193
---     end
-    
---   end
+
   
---   if GetCurrentMapIndex() == 14 then -- Check to see if we are inside the Cyrodiil zone
+
+  --         zoneBlobData = {
+--           texture = "AccurateWorldMap/blobs/tamriel-eastmarch.dds",
+--           xN = 0.4,
+--           yN = 0.4,
+--         },
+--         zonePolygonData = {
+--           -- generate polygon based on texture? edge detection? (for each zone that would be slooow on startup)
+--           -- way to move existing polygons? by zoneID?
+--         },
+--     },
+
+-- };
+
+
+-- local function parseWayshrines()
+
+--   for mapID, mapData in pairs(zoneData) do
+--     local wayshrines = mapData.wayshrines
     
-    
---   if nodeIndex == 236 then -- Imperial City Prison
---       normalizedX = 0.523
---       normalizedY = 0.382
+--     if (wayshrines ~= nil) then
+--       for wayshrineID, wayshrineData in pairs(wayshrines) do
+--         globalWayshrines[wayshrineID] = wayshrineData
+--       end
 --     end
-    
---     if nodeIndex == 247 then -- White Gold Tower
---       normalizedX = 0.497
---       normalizedY = 0.428
---     end
-    
---     if nodeIndex == 202 then --Northern Morrowind Wayshrine
---       name = "Northern Morrowind Gate Wayshrine"
---     end
-    
---     if nodeIndex == 203 then --Southern Morrowind Wayshrine
---       name = "Southern Morrowind Gate Wayshrine"
---     end
-    
---     if nodeIndex == 170 then --Northern Hammerfell Wayshrine
---       name = "Northern Hammerfell Gate Wayshrine"
---     end
-    
---     if nodeIndex == 199 then --Southern Hammerfell Wayshrine
---       name = "Southern Hammerfell Gate Wayshrine"
---     end
-    
---     if nodeIndex == 200 then --Eastern Elsweyr Wayshrine
---       name = "Eastern Elsweyr Gate Wayshrine"
---     end
-    
---     if nodeIndex == 201 then --Western Elsweyr Wayshrine
---       name = "Western Elsweyr Gate Wayshrine"
---     end
-    
 --   end
-  
--- 	return known, name, normalizedX, normalizedY, icon, glowIcon, poiType, isLocatedInCurrentMap, linkedCollectibleIsLocked, disabled
 -- end
-  
+
+
+
 
 function AWM.GetMapCustomMaxZoom()
     if not enabled then return _GetMapCustomMaxZoom() end
@@ -440,9 +384,6 @@ local optionsData = {
 local function OnAddonLoaded(event, addonName)
     if addonName ~= AWM.name then return end
     EVENT_MANAGER:UnregisterForEvent(AWM.name, EVENT_ADD_ON_LOADED)
-
-
-    parseWayshrines()
 
     MoveZones()
 
